29e43e3e3e735f715abad4f246395b10112500ae
David Silva <gestor.davidoliveira@gmail.com>
2025-12-11 22:30:35 -0300

    Projeto dom Aulas

---
+++ Projeto DOM/scripts.js
@@ -0,0 +1,56 @@
+// visualizar o conteudo do document
+//console.log(document)
+//obter o title da pagina
+/*console.log(document.title);
+// acessar o elemento pelo ID (Seletor ID)
+const guest = document.getElementById("guest-2")
+console.log(guest);
+
+// Mostra as propriedades do Objeto.
+console.dir(guest)
+
+//Acessar o Elemento com Class(Seletor Class)
+const guestsbyClass = document.getElementsByClassName("guest")
+console.log(guestsbyClass);
+//exibir o primeiro elemento da lista
+console.log(guestsbyClass.item (0))
+console.log(guestsbyClass[1])
+
+//Selecionar uma lista de elementos por tag
+const guestsByTag = document.getElementsByTagName("li")
+console.log(guestsByTag);
+
+//
+const guest1 = document.querySelector("#guest-1")
+console.log(guest1);
+
+//const guest2 = document.querySelector(".guest") // pra retornar os elementos na DOM
+const guest2 = document.querySelectorAll(".guest") // o Selector com o All retorna todos que ele encontra
+console.log(guest2);
+//
+
+// como manipular os elementos na DOM
+
+const guest3 = document.querySelector("#guest-1 ")
+console.log(guest3.textContent)// retorna o conteudo como Texto
+//guest3.textContent = "Joao" muda o nome entro do ID
+
+console.log(guest3.textContent)//Retornar o conteudo visivel e oculto
+console.log(guest3.innerText)//Retornar como texto sem Formatação (apenas o conteudo visivel)
+console.log(guest3.innerHTML)//Retorna o html como texto
+
+
+//Adciona a classe com JS no 
+const input = document.querySelector("#name")
+//input.classList.add("input-error")
+
+// Remove  a Classe
+//input.classList.remove("input-error")
+// se nao tiver a classe adiciona, se tiver remove
+//input.classList.toggle("input-error")
+
+const button = document.querySelector("button")
+// Modificar as Propriedades CSS do Elemento
+button.style.backgroundColor = "red"
+*/
+
